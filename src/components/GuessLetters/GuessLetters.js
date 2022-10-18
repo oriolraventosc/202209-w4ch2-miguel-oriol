9bb923c89f4dbfd392e4d9cea513b2e9746e67d1
Oriol Raventós <107557699+oriolraventosc@users.noreply.github.com>
2022-10-18 23:17:45 +0200

    Update GuessLetters.js

--- src/components/GuessLetters/GuessLetters.js
+++ src/components/GuessLetters/GuessLetters.js
@@ -3,19 +3,19 @@ import "./GuessLetters.css";
 const GuessLetters = ({ gameWord }) => {
   return (
     <ul className="guess-letters">
-      <li className={"guess-letter empty guess-letter"}>
+      <li className={"guess-letter empty"}>
         {gameWord[0].replace(gameWord[0], "")}
       </li>
-      <li className={"guess-letter empty guess-letter"}>{gameWord[1]}</li>
-      <li className={"guess-letter empty guess-letter"}>
+      <li className={"guess-letter"}>{gameWord[1]}</li>
+      <li className={"guess-letter empty"}>
         {gameWord[2].replace(gameWord[2], "")}
       </li>
-      <li className={"guess-letter empty guess-letter"}>{gameWord[3]}</li>
-      <li className={"guess-letter empty guess-letter"}>
+      <li className={"guess-letter"}>{gameWord[3]}</li>
+      <li className={"guess-letter empty"}>
         {gameWord[4].replace(gameWord[4], "")}
       </li>
-      <li className={"guess-letter empty guess-letter"}>{gameWord[5]}</li>
-      <li className={"guess-letter empty guess-letter"}>
+      <li className={"guess-letter"}>{gameWord[5]}</li>
+      <li className={"guess-letter empty"}>
         {gameWord[6].replace(gameWord[6], " ")}
       </li>
     </ul>
